8bad644c4d326cdab2c2072caaa1623c56e3685c
KishanWebDev <raik55488@gmail.com>
2024-09-18 03:06:31 +0530

    changed pdf name

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import "./Header.css"
-import Resume from "../assests/MERN.pdf"
+import Resume from "../assests/Kishan_MernStack_Resume.pdf"
 
 const roles = [
   'Full Stack Developer',
